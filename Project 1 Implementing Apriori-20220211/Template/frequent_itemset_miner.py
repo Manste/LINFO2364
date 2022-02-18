10acb5933c9934542f83bc72ec62fa0b90dc23d2
Manste <manuelle.ndamtang@yahoo.com>
2022-02-18 22:15:45 +0100

    try to unse numpy

--- Project 1 Implementing Apriori-20220211/Template/frequent_itemset_miner.py
+++ Project 1 Implementing Apriori-20220211/Template/frequent_itemset_miner.py
@@ -20,26 +20,28 @@ Do not change the signature of the apriori and alternative_miner methods as they
 
 __authors__ = "Group 8: Manuelle Ndamtang <manuelle.ndamtang@student.uclouvain.be>"
 """
-
+from itertools import combinations
+import numpy as np
 
 class Dataset:
 	"""Utility class to manage a dataset stored in a external file."""
 
 	def __init__(self, filepath):
 		"""reads the dataset file and initializes files"""
-		self.transactions = list()
+		temp_transactions = list()
 		self.items = set()
 
 		try:
 			lines = [line.strip() for line in open(filepath, "r")]
 			lines = [line for line in lines if line]  # Skipping blank lines
 			for line in lines:
-				transaction = list(map(int, line.split(" ")))
-				self.transactions.append(transaction)
+				transaction = np.asarray(line.split(" "), dtype=np.int64)
+				temp_transactions.append(transaction)
 				for item in transaction:
 					self.items.add(item)
 		except IOError as e:
 			print("Unable to read dataset file!\n" + e)
+		self.transactions = np.asarray(temp_transactions, dtype=object)
 
 	def trans_num(self):
 		"""Returns the number of transactions in the dataset"""
@@ -80,43 +82,38 @@ def check_frequencies(frequency_per_candidate, min_frequency):
 	for candidate, frequency in frequency_per_candidate.items():
 		if frequency >= min_frequency:
 			frequent_candidates.append(candidate)
-			print("{}  ({})".format(sorted(list(candidate)), frequency))
+			print("{}  ({})".format(candidate, frequency))
 	return frequent_candidates
 
 
 def frequencies(candidates, dataset):
-	"""Counting candidates using the naive process"""
 	items_frequencies = {}
 	if len(candidates) == 0 or candidates[0] is None: return dataset.trans_num()
 	for candidate in candidates:
 		"""For each transaction,we check if it contains the itemset."""
-		for transaction in dataset.transactions:
-			if candidate.issubset(transaction):
-				c = frozenset(candidate)
-				if c in items_frequencies.keys():
-					items_frequencies[c] += 1 / dataset.trans_num()
-				else:
-					items_frequencies[c] = 1 / dataset.trans_num()
-	return items_frequencies
+		filtered_transactions = dataset.transactions.apply(lambda row: items_frequencies[candidate] = 1/dataset.trans_num() if candidate in items_frequencies else items_frequencies[candidate] = items_frequencies[candidate] + 1/dataset.trans_num() )
+	temp_items = np.array(items_frequencies.keys()).reshape(-1, 1)
+	temp_frequencies = np.array(items_frequencies.values()).reshape(-1, 1)
+	print(items_frequencies)
+	return np.array((temp_items, temp_frequencies), axis=1)
 
 
 """
 We will generate candidate based on frequent itemset detected
 """
 def generate_candidates(dataset, level, last_candidates=[]):
-	from itertools import combinations
 	new_candidates = []
 	if level == 0:
 		new_candidates = [None]
 	elif level == 1:
 		for item in dataset.items:
-			new_candidates.append({item})
+			new_candidates.append([item])
 	else:
 		for itemset in combinations(last_candidates, level):
 			temp_parent = list(itemset)
 			lst = frozenset().union(*temp_parent)
 			if len(lst) == level:
-				new_candidates.append(lst)
+				new_candidates.append(list(lst))
 	return new_candidates
 
 
@@ -125,4 +122,4 @@ def alternative_miner(filepath, minFrequency):
 	# TODO: either second implementation of the apriori algorithm or implementation of the depth first search algorithm
 	print("Not implemented")
 
-apriori('../Datasets/chess.dat', 0.9)
+apriori('../Datasets/toy.dat', 0.9)
